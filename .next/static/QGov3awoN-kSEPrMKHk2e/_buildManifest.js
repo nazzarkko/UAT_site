8cb2be4f7eb23bea717d252ce101efabaf5fbfe5
artur-zhdan <78844452+artur-zhdan@users.noreply.github.com>
2023-12-12 19:34:07 +0200

    change nextjs to 12

---
+++ .next/static/QGov3awoN-kSEPrMKHk2e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-86dd9ee68b2782f0.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
