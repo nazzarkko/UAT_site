7ff9ae506df4ead8fb52c63d52c345bceb960a0d
artur-zhdan <78844452+artur-zhdan@users.noreply.github.com>
2023-12-21 23:24:48 +0200

    upd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4c2e00e4e85f0a8e"; }
+/******/ 		__webpack_require__.h = function() { return "758995ddfa5684c2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
