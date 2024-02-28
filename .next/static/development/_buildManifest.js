eb2d547a57de7ae5fe444f23b46b68e47dcb1257
artur-zhdan <78844452+artur-zhdan@users.noreply.github.com>
2024-02-28 13:45:20 +0200

    upd

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/test/[section]":["static\u002Fchunks\u002Fpages\u002Ftest\u002F[section].js"],sortedPages:["\u002F_app","\u002F_error","\u002Ftest\u002F[section]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
