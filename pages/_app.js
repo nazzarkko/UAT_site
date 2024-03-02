fcf0b96da9b2ad9b0032d3047b8d004fab84e47d
ukrainianarmytest <nazar.shyika.ir.2022@lpnu.ua>
2024-03-02 14:37:16 +0200

    Adding_chenges of radar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
 
-    return <Component {...pageProps} />
-
+    return (
+        <>
+          <head>
+            <title>UAT - Ukrainian Army Test</title>
+            <link rel="icon" href="/LOGO.png"  />
+          </head>
+          <Component {...pageProps} />
+        </>
+      );
 }
 
 export default MyApp
